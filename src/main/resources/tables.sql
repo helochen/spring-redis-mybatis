e4edd49579eec2905511cd1646c3bb3538b1ce1b
helochen <3334972898@qq.com>
2017-06-09 18:30:33 +0800

    添加mybatis初始化的sql

---
+++ src/main/resources/tables.sql
@@ -0,0 +1,12 @@
+create table userinfo(
+  id VARCHAR(32),
+  name VARCHAR(32),
+  password VARCHAR(32)
+);
+
+INSERT INTO userinfo (id,name ,mybatis.userinfo.password) VALUES ('1','chen','chen');
+INSERT INTO userinfo (id,name ,mybatis.userinfo.password) VALUES ('2','root','111111');
+INSERT INTO userinfo (id,name ,mybatis.userinfo.password) VALUES ('3','test','000000');
+INSERT INTO userinfo (id,name ,mybatis.userinfo.password) VALUES ('4','popo','000000');
+
+
